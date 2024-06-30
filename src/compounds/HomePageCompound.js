c1abfb3e057ef8c4b03472d802c41014a08fd244
Jomana ahmed <ojy.ahmed@outlook.com>
2024-06-30 07:02:58 +0200

    Adding Header.js file to the HomePageCompound.js file

--- src/compounds/HomePageCompound.js
+++ src/compounds/HomePageCompound.js
@@ -1,11 +1,13 @@
 import React from "react";
 import styled from "styled-components";
 import Navbar from "../components/HomePage/Navbar";
+import Header from "../components/HomePage/Header";
 
 function HomePageCompound() {
   return (
     <Wrapper>
       <Navbar />
+      <Header />
     </Wrapper>
   );
 }
