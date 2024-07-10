0c8f254aae7abc00f87eec44fb30f9a3bc8a38fe
Jomana ahmed <ojy.ahmed@outlook.com>
2024-07-10 20:58:50 +0200

    Creating AboutUs.js file

---
+++ src/components/HomePage/AboutUs.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+function AboutUs() {
+  return (
+    <Wrapper>
+      
+    </Wrapper>
+  );
+}
+
+export const Wrapper = styled.div``;
+
+export default AboutUs;
