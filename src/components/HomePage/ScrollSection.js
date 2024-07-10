e0968e8d3e4a5dddc4c94c1a28f00956f9211672
Jomana ahmed <ojy.ahmed@outlook.com>
2024-07-10 21:53:40 +0200

    Creating ScrollSection.js file

---
+++ src/components/HomePage/ScrollSection.js
@@ -0,0 +1,20 @@
+import React from "react";
+import styled from "styled-components";
+
+function ScrollSection() {
+  return (
+    <Wrapper>
+      
+    </Wrapper>
+  );
+}
+
+export const Wrapper = styled.div`
+  background-image: url("/imgs/about-us-bg.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-attachment: fixed;
+  height: 673px;
+`;
+
+export default ScrollSection;
