2836cb29e9e8544be77ac6f2bc9aaae5bf3cce89
Jomana ahmed <ojy.ahmed@outlook.com>
2024-07-12 21:02:48 +0200

    Creating FindUs.js file

---
+++ src/components/HomePage/FindUs.js
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+
+function FindUs() {
+  return (
+    <Wrapper>
+      
+    </Wrapper>
+  );
+}
+
+export const Wrapper = styled.div`
+  
+`;
+
+export default FindUs;
