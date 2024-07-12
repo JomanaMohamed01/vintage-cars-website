3c802476c500c76cc849e1564730cde439ad48d8
Jomana ahmed <ojy.ahmed@outlook.com>
2024-07-12 21:00:38 +0200

    Creating the Scroll down section in the Home page ( with Responsive design )

--- src/components/HomePage/ScrollSection.js
+++ src/components/HomePage/ScrollSection.js
@@ -4,17 +4,33 @@ import styled from "styled-components";
 function ScrollSection() {
   return (
     <Wrapper>
-      
+      <MainWrapper>
+        <Image src="/imgs/jade-and-andy-bg.jpg" />
+      </MainWrapper>
     </Wrapper>
   );
 }
 
 export const Wrapper = styled.div`
-  background-image: url("/imgs/about-us-bg.jpg");
+  /* position: relative; */
+`;
+
+export const MainWrapper = styled.div`
+  /* background-image: url("/imgs/jade-and-andy-bg.jpg");
   background-repeat: no-repeat;
-  background-size: cover;
+  background-size: contain;
+  background-position: center;
   background-attachment: fixed;
-  height: 673px;
+  height: 520px;
+  border: 1px solid red; */
+  /* position: fixed; */
+`;
+
+export const Image = styled.img`
+  width: 100%;
+  height: auto;
+  /* position: fixed; */
+  /* border: 1px solid transparent; */
 `;
 
 export default ScrollSection;
