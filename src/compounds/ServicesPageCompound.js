d69382ddf409f1f978fd5b38af313a28a76bbcf4
Jomana ahmed <ojy.ahmed@outlook.com>
2024-07-13 18:42:38 +0200

    Adding Navbar.js file to the ServicesPageCompound.js file

--- src/compounds/ServicesPageCompound.js
+++ src/compounds/ServicesPageCompound.js
@@ -1,10 +1,11 @@
 import React from "react";
 import styled from "styled-components";
+import Navbar from "../components/ServicesPage/Navbar";
 
 function ServicesPageCompound() {
   return (
     <Wrapper>
-      
+      <Navbar />
     </Wrapper>
   );
 }
