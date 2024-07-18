1a01273a1fd3d318ec3a04b3368edcd6d7d0cd94
Jomana ahmed <ojy.ahmed@outlook.com>
2024-07-18 03:09:55 +0200

    Adding Navbar.js file to the OurWorkCompound.js file

--- src/compounds/OurWorkCompound.js
+++ src/compounds/OurWorkCompound.js
@@ -1,10 +1,11 @@
 import React from "react";
 import styled from "styled-components";
+import Navbar from "../components/OurWork/Navbar";
 
 function OurWorkCompound() {
   return (
     <Wrapper>
-      
+      <Navbar />
     </Wrapper>
   );
 }
