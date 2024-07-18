8a8792223ba7fe9b64177cf4867d6d851c91ab78
Jomana ahmed <ojy.ahmed@outlook.com>
2024-07-18 03:29:21 +0200

    Adding Footer.js file to the AboutUsCompound.js file

--- src/compounds/AboutUsCompound.js
+++ src/compounds/AboutUsCompound.js
@@ -2,12 +2,14 @@ import React from "react";
 import styled from "styled-components";
 import Navbar from "../components/AboutUs/Navbar";
 import AboutUs from "../components/AboutUs/AboutUs";
+import Footer from "../components/AboutUs/Footer";
 
 function AboutUsCompound() {
   return (
     <Wrapper>
       <Navbar />
       <AboutUs />
+      <Footer />
     </Wrapper>
   );
 }
