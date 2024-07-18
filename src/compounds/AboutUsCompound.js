5ad65d5c92c0fd9603b95a62f8fa726d0944e970
Jomana ahmed <ojy.ahmed@outlook.com>
2024-07-18 03:24:59 +0200

    Adding Navbar.js file to the AboutUsCompound.js file

--- src/compounds/AboutUsCompound.js
+++ src/compounds/AboutUsCompound.js
@@ -1,10 +1,11 @@
 import React from "react";
 import styled from "styled-components";
+import Navbar from "../components/AboutUs/Navbar";
 
 function AboutUsCompound() {
   return (
     <Wrapper>
-      
+      <Navbar />
     </Wrapper>
   );
 }
