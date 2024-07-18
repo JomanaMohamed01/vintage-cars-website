e259489c48c4e13155d784a2f802e1f7cd36cc7a
Jomana ahmed <ojy.ahmed@outlook.com>
2024-07-18 03:36:59 +0200

    Adding Navbar.js file to the ContactCompound.js file

--- src/compounds/ContactCompound.js
+++ src/compounds/ContactCompound.js
@@ -1,10 +1,11 @@
 import React from "react";
 import styled from "styled-components";
+import Navbar from "../components/ContactPage/Navbar";
 
 function ContactCompound() {
   return (
     <Wrapper>
-      
+      <Navbar />
     </Wrapper>
   );
 }
