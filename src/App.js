1ba43452c2c4d14574823cd6bc1fe0021307fe1e
Jomana ahmed <ojy.ahmed@outlook.com>
2024-07-18 03:07:39 +0200

    Commenting unnecessary code in the App.js file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 // import HomePageCompound from './compounds/HomePageCompound';
-import ServicesPageCompound from './compounds/ServicesPageCompound';
+// import ServicesPageCompound from './compounds/ServicesPageCompound';
 
 function App() {
   return (
     <div className="App">
       {/* <HomePageCompound /> */}
-      <ServicesPageCompound />
+      {/* <ServicesPageCompound /> */}
     </div>
   );
 }
