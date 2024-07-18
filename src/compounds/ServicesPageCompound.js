03e1b3866fd30628b1a8d829787850850bf59891
Jomana ahmed <ojy.ahmed@outlook.com>
2024-07-18 02:57:54 +0200

    Adding Services.js file to the ServicesPageCompound.js file

--- src/compounds/ServicesPageCompound.js
+++ src/compounds/ServicesPageCompound.js
@@ -1,11 +1,13 @@
 import React from "react";
 import styled from "styled-components";
 import Navbar from "../components/ServicesPage/Navbar";
+import Services from "../components/ServicesPage/Services";
 
 function ServicesPageCompound() {
   return (
     <Wrapper>
       <Navbar />
+      <Services />
     </Wrapper>
   );
 }
