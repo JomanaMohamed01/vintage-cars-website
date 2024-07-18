0eb42b7e189a77183381e2f34aaf9b1ce09071dc
Jomana ahmed <ojy.ahmed@outlook.com>
2024-07-18 03:18:14 +0200

    Creating the AboutUsCompound.js file

---
+++ src/compounds/AboutUsCompound.js
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+
+function AboutUsSection() {
+  return (
+    <Wrapper>
+      
+    </Wrapper>
+  );
+}
+
+export const Wrapper = styled.div`
+  
+`;
+
+export default AboutUsSection;
