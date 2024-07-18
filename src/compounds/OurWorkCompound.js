4bccc8567780a74fb24c7f53be1b9c9a5e50692b
Jomana ahmed <ojy.ahmed@outlook.com>
2024-07-18 03:12:09 +0200

    Adding OurWork.js file to the OurWorkCompound.js file

--- src/compounds/OurWorkCompound.js
+++ src/compounds/OurWorkCompound.js
@@ -1,11 +1,13 @@
 import React from "react";
 import styled from "styled-components";
 import Navbar from "../components/OurWork/Navbar";
+import OurWork from "../components/OurWork/OurWork";
 
 function OurWorkCompound() {
   return (
     <Wrapper>
       <Navbar />
+      <OurWork />
     </Wrapper>
   );
 }
