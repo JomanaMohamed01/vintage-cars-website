01592e7398afbde1dc182dda73123e8bd9ec8973
Jomana ahmed <ojy.ahmed@outlook.com>
2024-07-18 03:34:16 +0200

    Creating ContactCompound.js file

---
+++ src/compounds/ContactCompound.js
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+
+function ContactCompound() {
+  return (
+    <Wrapper>
+      
+    </Wrapper>
+  );
+}
+
+export const Wrapper = styled.div`
+  
+`;
+
+export default ContactCompound;
