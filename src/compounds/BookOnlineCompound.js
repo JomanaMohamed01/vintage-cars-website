71da1ed104959d9182b45b9f08f8c7cb4e40d701
Jomana ahmed <ojy.ahmed@outlook.com>
2024-07-18 04:09:47 +0200

    Adding the BookOnline.js file to the BookOnlineCompound.js file

--- src/compounds/BookOnlineCompound.js
+++ src/compounds/BookOnlineCompound.js
@@ -1,11 +1,13 @@
 import React from "react";
 import styled from "styled-components";
 import Navbar from "../components/BookOnlinePage/Navbar";
+import BookOnline from "../components/BookOnlinePage/BookOnline";
 
 function BookOnlineCompound() {
   return (
     <Wrapper>
       <Navbar />
+      <BookOnline />
     </Wrapper>
   );
 }
