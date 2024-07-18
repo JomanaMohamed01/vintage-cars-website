28d1fd73b3237e69cce9a8196d873d1c5b395a80
Jomana ahmed <ojy.ahmed@outlook.com>
2024-07-18 03:27:46 +0200

    Adding AboutUs.js file to the AboutUsCompound.js file

--- src/compounds/AboutUsCompound.js
+++ src/compounds/AboutUsCompound.js
@@ -1,11 +1,13 @@
 import React from "react";
 import styled from "styled-components";
 import Navbar from "../components/AboutUs/Navbar";
+import AboutUs from "../components/AboutUs/AboutUs";
 
 function AboutUsCompound() {
   return (
     <Wrapper>
       <Navbar />
+      <AboutUs />
     </Wrapper>
   );
 }
