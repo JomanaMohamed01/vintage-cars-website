5fd4251f449e984734fb8577a69523bc517a9cf9
Jomana ahmed <ojy.ahmed@outlook.com>
2024-07-18 03:14:01 +0200

    Adding the Footer.js file to the OurWorkCompound.js file

--- src/compounds/OurWorkCompound.js
+++ src/compounds/OurWorkCompound.js
@@ -2,12 +2,14 @@ import React from "react";
 import styled from "styled-components";
 import Navbar from "../components/OurWork/Navbar";
 import OurWork from "../components/OurWork/OurWork";
+import Footer from "../components/OurWork/Footer";
 
 function OurWorkCompound() {
   return (
     <Wrapper>
       <Navbar />
       <OurWork />
+      <Footer />
     </Wrapper>
   );
 }
