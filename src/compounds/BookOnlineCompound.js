7b18ff92f0cecbde3514e8b4053fed561b1455fd
Jomana ahmed <ojy.ahmed@outlook.com>
2024-07-18 04:03:17 +0200

    Creating BookOnlineCompound.js file

---
+++ src/compounds/BookOnlineCompound.js
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+import Navbar from "../components/HomePage/Navbar";
+
+function BookOnlineCompound() {
+  return (
+    <Wrapper>
+      
+    </Wrapper>
+  );
+}
+
+export const Wrapper = styled.div`
+  
+`;
+
+export default BookOnlineCompound;
