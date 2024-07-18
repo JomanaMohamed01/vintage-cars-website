d8dd2f35456934f5f3df0dc2f2c439151165533f
Jomana ahmed <ojy.ahmed@outlook.com>
2024-07-18 03:01:40 +0200

    Adding Footer.js file to the ServicesPageCompound.js file

--- src/compounds/ServicesPageCompound.js
+++ src/compounds/ServicesPageCompound.js
@@ -2,12 +2,14 @@ import React from "react";
 import styled from "styled-components";
 import Navbar from "../components/ServicesPage/Navbar";
 import Services from "../components/ServicesPage/Services";
+import Footer from "../components/ServicesPage/Footer";
 
 function ServicesPageCompound() {
   return (
     <Wrapper>
       <Navbar />
       <Services />
+      <Footer />
     </Wrapper>
   );
 }
