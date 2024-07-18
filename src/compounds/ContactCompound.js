0232013f45da4109a2529fcaf77191048c7eb440
Jomana ahmed <ojy.ahmed@outlook.com>
2024-07-18 03:58:29 +0200

    Adding Footer.js file to the ContactCompound.js file

--- src/compounds/ContactCompound.js
+++ src/compounds/ContactCompound.js
@@ -2,12 +2,14 @@ import React from "react";
 import styled from "styled-components";
 import Navbar from "../components/ContactPage/Navbar";
 import Contact from "../components/ContactPage/Contact";
+import Footer from "../components/ContactPage/Footer";
 
 function ContactCompound() {
   return (
     <Wrapper>
       <Navbar />
       <Contact />
+      <Footer />
     </Wrapper>
   );
 }
