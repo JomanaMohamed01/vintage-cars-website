a1ccdb263b4ad6874d648da5e284c62972c50fa6
Jomana ahmed <ojy.ahmed@outlook.com>
2024-07-20 22:40:28 +0200

    Adding Footer.js file to the BookOnlineCompound.js file

--- src/compounds/BookOnlineCompound.js
+++ src/compounds/BookOnlineCompound.js
@@ -2,18 +2,18 @@ import React from "react";
 import styled from "styled-components";
 import Navbar from "../components/BookOnlinePage/Navbar";
 import BookOnline from "../components/BookOnlinePage/BookOnline";
+import Footer from "../components/BookOnlinePage/Footer";
 
 function BookOnlineCompound() {
   return (
     <Wrapper>
       <Navbar />
       <BookOnline />
+      <Footer />
     </Wrapper>
   );
 }
 
-export const Wrapper = styled.div`
-  
-`;
+export const Wrapper = styled.div``;
 
 export default BookOnlineCompound;
