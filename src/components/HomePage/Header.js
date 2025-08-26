aabcb7f338ff1d4538ea92d935c589466b5d6435
Jomana ahmed <ojy.ahmed@outlook.com>
2025-08-26 17:55:29 +0200

    Fiixng the padding-bottom

--- src/components/HomePage/Header.js
+++ src/components/HomePage/Header.js
@@ -5,94 +5,6 @@ import { Facebook } from "@styled-icons/simple-icons/Facebook"
 import { Pinterest } from "@styled-icons/bootstrap/Pinterest"
 import { InstagramWithCircle } from "@styled-icons/entypo-social/InstagramWithCircle"
 
-const Wrapper = styled.div`
-  background-image: url("/imgs/about-us-bg.jpg");
-  background-repeat: no-repeat;
-  background-size: cover;
-  background-attachment: fixed;
-  height: auto;
-  padding-top: 130px;
-  padding-bottom: 150px;
-  @media screen and (max-width: 768px) {
-    background-attachment: scroll;
-    background-size: cover;
-    background-position: center;
-    border-bottom: none;
-    margin-bottom: 0;
-    padding-bottom: 0;
-  }
-  @media screen and (max-width: 550px) {
-    /* height: 431px; */
-    padding-top: 50px;
-    padding-bottom: 100px;
-    margin-bottom: 0;
-  }
-`;
-const MainWrapper = styled.div``;
-const TextWrapper = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-`;
-const ToolsIcon = styled(Tools)`
-  color: #ffffff;
-  width: 139px;
-  height: 139px;
-  border: 3px solid #ffffff;
-  border-radius: 100px;
-  padding: 20px;
-  margin-bottom: -60px;
-  @media screen and (max-width: 550px) {
-    width: 110px;
-    height: 110px;
-    margin-bottom: -50px;
-  }
-`;
-const BigHeader = styled.p`
-  color: #ffffff;
-  font-size: 68px;
-  margin-bottom: -20px;
-  @media screen and (max-width: 550px) {
-    font-size: 51px;
-    margin-bottom: -20px;
-  }
-`;
-const SmallHeader = styled.p`
-  color: #ffffff;
-  font-size: 19px;
-  letter-spacing: 2px;
-  @media screen and (max-width: 550px) {
-    font-size: 13px;
-  }
-`;
-const SocialBarWrapper = styled.div`
-  width: 150px;
-  display: flex;
-  justify-content: space-between;
-  margin-top: 600px;
-  margin-left: auto;
-  margin-right: auto;
-`;
-const FacebookIcon = styled(Facebook)`
-  color: #ffffff;
-  width: 40px;
-  height: 40px;
-  cursor: pointer;
-`;
-const PinterestIcon = styled(Pinterest)`
-  color: #ffffff;
-  width: 40px;
-  height: 40px;
-  cursor: pointer;
-`;
-const InstagramIcon = styled(InstagramWithCircle)`
-  color: #ffffff;
-  width: 40px;
-  height: 40px;
-  cursor: pointer;
-`;
-
 function Header() {
   return (
     <Wrapper>
@@ -109,7 +21,92 @@ function Header() {
         </SocialBarWrapper>
       </MainWrapper>
     </Wrapper>
-  );
+  )
 }
 
-export default Header;
+const Wrapper = styled.div`
+  background-image: url("/imgs/about-us-bg.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-attachment: fixed;
+  height: auto;
+  padding-top: 130px;
+  padding-bottom: 150px;
+
+  @media screen and (max-width: 768px) {
+    background-attachment: scroll;
+    background-size: cover;
+    background-position: center;
+    border-bottom: none;
+    margin-bottom: 0;
+  }
+  @media screen and (max-width: 550px) {
+    padding-top: 50px;
+  }
+`
+const MainWrapper = styled.div``
+const TextWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`
+const ToolsIcon = styled(Tools)`
+  color: #ffffff;
+  width: 139px;
+  height: 139px;
+  border: 3px solid #ffffff;
+  border-radius: 100px;
+  padding: 20px;
+  margin-bottom: -60px;
+  @media screen and (max-width: 550px) {
+    width: 110px;
+    height: 110px;
+    margin-bottom: -50px;
+  }
+`
+const BigHeader = styled.p`
+  color: #ffffff;
+  font-size: 68px;
+  margin-bottom: -20px;
+  @media screen and (max-width: 550px) {
+    font-size: 51px;
+    margin-bottom: -20px;
+  }
+`
+const SmallHeader = styled.p`
+  color: #ffffff;
+  font-size: 19px;
+  letter-spacing: 2px;
+  @media screen and (max-width: 550px) {
+    font-size: 13px;
+  }
+`
+const SocialBarWrapper = styled.div`
+  width: 150px;
+  display: flex;
+  justify-content: space-between;
+  margin-top: 600px;
+  margin-left: auto;
+  margin-right: auto;
+`
+const FacebookIcon = styled(Facebook)`
+  color: #ffffff;
+  width: 40px;
+  height: 40px;
+  cursor: pointer;
+`
+const PinterestIcon = styled(Pinterest)`
+  color: #ffffff;
+  width: 40px;
+  height: 40px;
+  cursor: pointer;
+`
+const InstagramIcon = styled(InstagramWithCircle)`
+  color: #ffffff;
+  width: 40px;
+  height: 40px;
+  cursor: pointer;
+`
+
+export default Header
